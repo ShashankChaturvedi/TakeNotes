492d97e07b966bdacab881478496cb8b17dbbd6e
ShashankChaturvedi <gshashank1998@gmail.com>
2023-08-03 17:03:58 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ export default function App() {
                     className="first-note" 
                     onClick={createNewNote}
                 >
-                    Create one now
+                    Create a Note
                 </button>
             </div>
             
